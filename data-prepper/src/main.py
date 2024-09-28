8dac9b8e53762d63dca0619b98b20913fee34ba8
heimdall <arth.gassner@gmail.com>
2024-09-28 16:05:27 +0200

    Cleanup Dockerized data-prepper

---
+++ data-prepper/src/main.py
@@ -0,0 +1,34 @@
+from fastapi import BackgroundTasks, FastAPI
+from dotenv import load_dotenv
+import os
+
+from .data_loader import DataLoader
+from .data_cleaner import DataCleaner
+from .feature_extractor import FeatureExtractor
+
+load_dotenv(dotenv_path='.env')
+
+app = FastAPI(title="Data preparation")
+
+def prep_data(enstoe_api_key: str):
+
+    # Update the bronze-layer data
+    data_loader = DataLoader(enstoe_api_key=enstoe_api_key)
+    data_loader.update_df(out_df_filepath="data/bronze/df.parquet")
+
+    # Clean the bronze-layer data
+    DataCleaner.clean(
+        in_df_filepath='data/bronze/df.parquet',
+        out_df_filepath='data/silver/df.parquet',
+    )
+
+    # Extract features
+    FeatureExtractor.extract_features(
+        in_df_filepath='data/silver/df.parquet',
+        out_df_filepath='data/gold/df.parquet',
+    )
+
+@app.get("/prep-data")
+async def get_prep_data(background_tasks: BackgroundTasks):
+    background_tasks.add_task(prep_data, enstoe_api_key=os.getenv('ENTSOE_API_KEY'))
+    return {"message": "Data preparation started"} 
