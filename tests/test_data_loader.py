5273a7141bc16cde86718cdbf353d92a93466e1f
heimdall <arth.gassner@gmail.com>
2024-09-30 22:04:34 +0200

    Add test

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -4,7 +4,7 @@ import pytest
 from model_server.data_loader import DataLoader
 
 
-def test__get_latest_ts_with_actual_load():
+def test__get_latest_ts_with_actual_load__empty():
 
     # Empty dataframe should return 2014-01-01
     excepted_ts = pd.Timestamp("20140101", tz="Europe/Zurich")
