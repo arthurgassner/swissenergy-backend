e1fea9789fadbe2740993cdcf87e0949256ba49f
heimdall <arth.gassner@gmail.com>
2024-09-30 22:19:49 +0200

    Improve logging

--- model_server/data_loader.py
+++ model_server/data_loader.py
@@ -1,9 +1,12 @@
+import logging
 from datetime import datetime
 from pathlib import Path
 
 import pandas as pd
 from entsoe import EntsoePandasClient
 from entsoe.exceptions import NoMatchingDataError
+
+logger = logging.getLogger(__name__)
 
 
 class DataLoader:
@@ -16,6 +19,10 @@ class DataLoader:
             entsoe_api_key (str): API key used to download data from the ENTSO-E servers.
                                   See https://transparency.entsoe.eu/content/static_content/Static%20content/web%20api/Guide.html
         """
+        if entsoe_api_key is None:
+            logger.error(f"Missing `entsoe_api_key`.")
+            raise ValueError
+
         self._entsoe_pandas_client = EntsoePandasClient(
             api_key=entsoe_api_key
         )  # Get API key through website, after kindly asking the support
@@ -66,7 +73,7 @@ class DataLoader:
                 country_code="CH", start=start_ts, end=end_ts
             )
         except NoMatchingDataError:
-            print(f"No data available between {start_ts} -> {end_ts}")
+            logger.warning(f"No data available between {start_ts} -> {end_ts}")
 
         # Append the newly-fetched data to the current data
         current_df = pd.concat([current_df, fetched_df], axis=0)
