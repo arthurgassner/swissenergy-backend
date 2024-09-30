73f2101bdf2f2a703e2803ddbb2ed9fcbe3495fc
heimdall <arth.gassner@gmail.com>
2024-09-30 22:14:29 +0200

    Add test

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -29,3 +29,29 @@ def test__get_latest_ts_with_actual_load__missing_actual_load():
     # Then
     excepted_ts = pd.Timestamp("20140101 00:00", tz="Europe/Zurich")
     assert excepted_ts == ts
+
+
+def test__get_latest_ts_with_actual_load():
+    """Dataframe with an 'Actual Load' should return the datetime of that row"""
+
+    # Given a df with no 'Actual Load' values
+    df = pd.DataFrame(
+        {
+            "Forecasted Load": [7890.0, np.nan, np.nan],
+            "Actual Load": [np.nan, 7890.0, np.nan],
+        },
+        index=pd.DatetimeIndex(
+            [
+                pd.Timestamp("20240101 23:45", tz="Europe/Zurich"),
+                pd.Timestamp("20240201 23:45", tz="Europe/Zurich"),
+                pd.Timestamp("20240301 23:45", tz="Europe/Zurich"),
+            ]
+        ),
+    )
+
+    # When
+    ts = DataLoader.get_latest_ts_with_actual_load(df=df)
+
+    # Then
+    excepted_ts = pd.Timestamp("20240201 23:45", tz="Europe/Zurich")
+    assert excepted_ts == ts
